a268f6cbfcd0b3c3afdf138021b07fdf499d4bae
Norbert Eke <norberte@users.noreply.github.com>
2019-04-03 11:00:14 -0400

    Updating the SQL script for table pruning

--- DB creation/SO-GH_table_pruning.sql
+++ DB creation/SO-GH_table_pruning.sql
@@ -9,7 +9,7 @@ ALTER TABLE commits_linked ADD CONSTRAINT `commits_linked_fk1` FOREIGN KEY (auth
 ALTER TABLE commits_linked ADD CONSTRAINT `commits_linked_fk2` FOREIGN KEY (committer_id) REFERENCES GH_users (id);
 ALTER TABLE commits_linked ADD CONSTRAINT `commits_linked_fk3` FOREIGN KEY (project_id) REFERENCES projects_linked (GH_project_id);
 DROP TABLE commits;
--- until here
+
 
 -- filter project_members table --> keep only project_members with repo_id that are in the projects_linked table
 CREATE TABLE project_members_linked AS
@@ -61,12 +61,11 @@ ALTER TABLE project_topics_linked ADD CONSTRAINT `project_topics_linked_ppk` PRI
 ALTER TABLE project_topics_linked ADD CONSTRAINT `project_topics_linked_fk` FOREIGN KEY (project_id) REFERENCES projects_linked (GH_project_id);
 DROP TABLE project_topics;
 
-
-
--- to be run:
+-- filter commit_comments table --> keep only commit_comments with commit_id that are in the commit_linked table
 CREATE TABLE commit_comments_linked AS
 SELECT * FROM commit_comments
 WHERE commit_id IN (SELECT id FROM commits_linked);
+
 
 ALTER TABLE commit_comments_linked ADD CONSTRAINT `commit_comments_linked_pk` PRIMARY KEY (id);
 ALTER TABLE commit_comments_linked ADD CONSTRAINT `commit_comments_linked_fk1` FOREIGN KEY (commit_id) REFERENCES commits_linked (id);
@@ -74,24 +73,26 @@ ALTER TABLE commit_comments_linked ADD CONSTRAINT `commit_comments_linked_fk2` F
 DROP TABLE commit_comments;
 
 
-
+-- filter commit_parents table --> keep only commit_parents with commit_id that are in the commit_linked table
+CREATE INDEX `commits_id_linked` ON commits_linked (id);
 CREATE TABLE commit_parents_linked AS
 SELECT * FROM commit_parents
-WHERE commit_id IN (SELECT id FROM commits_linked) OR parent_id IN (SELECT id FROM commits_linked);
+WHERE commit_id IN (SELECT id FROM commits_linked) AND parent_id IN (SELECT id FROM commits_linked);
 
-ALTER TABLE commit_parents_linked ADD CONSTRAINT `commit_parents_linked_fk1` FOREIGN KEY (commit_id) REFERENCES commits_linked (id);
+DELETE FROM commit_parents WHERE commit_id NOT IN (SELECT id FROM commits_linked) AND parent_id NOT IN (SELECT id FROM commits_linked); -- did not finish running
+
+
+ALTER TABLE commit_parents_linked ADD CONSTRAINT `commit_parents_linked_fk1` FOREIGN KEY (commit_id) REFERENCES commits_linked (id);  
 ALTER TABLE commit_parents_linked ADD CONSTRAINT `commit_parents_linked_fk2` FOREIGN KEY (parent_id) REFERENCES commits_linked (id);
 DROP TABLE commit_parents;
 
 
-
-
-CREATE TABLE tempTable1 AS SELECT id FROM commits_linked;
-CREATE TABLE tempTable2 AS SELECT DISTINCT GH_project_id FROM projects_linked;
-
+-- filter pull_requests table --> keep only commit_id and project_id that are in the commits_linked and project_linked tables
 CREATE TABLE pull_requests_linked AS
 SELECT * FROM pull_requests
-WHERE (head_commit_id IN tempTable1 OR base_commit_id IN tempTable1) AND (head_repo_id IN tempTable2 OR base_repo_id IN tempTable2);
+WHERE head_commit_id IN (SELECT id FROM commits_linked) and base_commit_id IN (SELECT id FROM commits_linked) 
+and head_repo_id IN (SELECT DISTINCT GH_project_id FROM projects_linked) and base_repo_id IN (SELECT DISTINCT GH_project_id FROM projects_linked);
+
 
 ALTER TABLE pull_requests_linked ADD CONSTRAINT `pull_requests_linked_pk` PRIMARY KEY (`id`);
 ALTER TABLE pull_requests_linked ADD CONSTRAINT `pull_requests_linked_fk1` FOREIGN KEY (`head_repo_id`) REFERENCES projects_linked (GH_project_id);
@@ -99,3 +100,99 @@ ALTER TABLE pull_requests_linked ADD CONSTRAINT `pull_requests_linked_fk2` FOREI
 ALTER TABLE pull_requests_linked ADD CONSTRAINT `pull_requests_linked_fk3` FOREIGN KEY (`head_commit_id`) REFERENCES commits_linked (id);
 ALTER TABLE pull_requests_linked ADD CONSTRAINT `pull_requests_linked_fk4` FOREIGN KEY (`base_commit_id`) REFERENCES commits_linked (id);
 DROP TABLE pull_requests;
+
+
+-- filter project_commits table --> keep only commit_id and project_id that are in the commits_linked and project_linked tables
+CREATE TABLE project_commits_linked AS SELECT project_id, id FROM commits_linked; 
+ALTER TABLE project_commits_linked ADD CONSTRAINT `project_commits_linked_fk1` FOREIGN KEY (project_id) REFERENCES projects_linked (GH_project_id);
+ALTER TABLE project_commits_linked ADD CONSTRAINT `project_commits_linked_fk2` FOREIGN KEY (id) REFERENCES commits_linked (id); -- running now
+
+
+
+-- filter pull_request_comments table --> keep only commit_id and pull_request_id that are in the commits_linked and pull_requests_linked tables
+CREATE TABLE pull_request_comments_linked AS
+SELECT * FROM pull_request_comments
+WHERE pull_request_id IN (SELECT id FROM pull_requests_linked) and commit_id IN (SELECT id FROM commits_linked);
+
+
+ 
+ALTER TABLE pull_request_comments_linked ADD CONSTRAINT `pull_request_comments_linked_fk1` FOREIGN KEY (pull_request_id) REFERENCES pull_requests_linked(id);
+ALTER TABLE pull_request_comments_linked ADD CONSTRAINT `pull_request_comments_linked_fk2` FOREIGN KEY (user_id) REFERENCES GH_users (id); 
+ALTER TABLE pull_request_comments_linked ADD CONSTRAINT `pull_request_comments_linked_fk3` FOREIGN KEY (commit_id) REFERENCES commits_linked (id);
+DROP TABLE pull_request_comments;	
+	
+	
+	
+-- filter pull_request_commits table --> keep only commit_id and pull_request_id that are in the commits_linked and pull_requests_linkes tables
+CREATE TABLE pull_request_commits_linked AS
+SELECT * FROM pull_request_commits
+WHERE pull_request_id IN (SELECT id FROM pull_requests_linked) and commit_id IN (SELECT id FROM commits_linked);
+
+
+ALTER TABLE pull_request_commits_linked ADD CONSTRAINT `pull_request_commits_linked_pk` PRIMARY KEY (pull_request_id, commit_id);
+ALTER TABLE pull_request_commits_linked ADD CONSTRAINT `pull_request_commits_linked_fk1` FOREIGN KEY (pull_request_id) REFERENCES pull_requests_linked(id);
+ALTER TABLE pull_request_commits_linked ADD CONSTRAINT `pull_request_commits_linked_fk2` FOREIGN KEY (commit_id) REFERENCES commits_linked(id); -- does not run
+DROP TABLE pull_request_commits;
+
+	
+	
+-- filter pull_request_history table --> keep only pull_request_id that are in the pull_requests_linkes table	
+CREATE TABLE pull_request_history_linked AS
+SELECT * FROM pull_request_history
+WHERE pull_request_id IN (SELECT id FROM pull_requests_linked);
+
+
+ALTER TABLE pull_request_history_linked ADD CONSTRAINT `pull_request_history_linked_pk` PRIMARY KEY (id);
+ALTER TABLE pull_request_history_linked ADD CONSTRAINT `pull_request_history_linked_fk1` FOREIGN KEY (pull_request_id) REFERENCES pull_requests_linked(id);
+ALTER TABLE pull_request_history_linked ADD CONSTRAINT `pull_request_history_linked_fk2` FOREIGN KEY (actor_id) REFERENCES GH_users(id);  -- does not run
+DROP TABLE pull_request_history;
+
+
+-- filter issues table --> keep only pull_request_id and repo_id that are in the pull_requests_linkes and projects_linked tables
+CREATE TABLE issues_linked AS
+SELECT * FROM issues
+WHERE repo_id IN (SELECT id FROM projects_linked) AND pull_request_id IN (SELECT id FROM pull_requests_linked);
+
+
+ALTER TABLE issues_linked ADD CONSTRAINT `issues_linked_pk` PRIMARY KEY (id);
+ALTER TABLE issues_linked ADD CONSTRAINT `issues_linked_fk1` FOREIGN KEY (repo_id) REFERENCES projects_linked(id); -- does not run
+ALTER TABLE issues_linked ADD CONSTRAINT `issues_linked_fk2` FOREIGN KEY (reporter_id) REFERENCES GH_users(id); -- does not run
+ALTER TABLE issues_linked ADD CONSTRAINT `issues_linked_fk3` FOREIGN KEY (assignee_id) REFERENCES GH_users(id);
+ALTER TABLE issues_linked ADD CONSTRAINT `issues_linked_fk4` FOREIGN KEY (pull_request_id) REFERENCES pull_requests_linked(id);
+DROP TABLE issues;
+
+
+
+-- filter issue_comments table --> keep only issue_id that are in the issues_linked table
+CREATE TABLE issue_comments_linked AS
+SELECT * FROM issue_comments
+WHERE issue_id IN (SELECT id FROM issues_linked);
+
+
+ALTER TABLE issue_comments_linked ADD CONSTRAINT `issue_comments_linked_fk1` FOREIGN KEY (issue_id) REFERENCES issues_linked(id); -- does not run
+ALTER TABLE issue_comments_linked ADD CONSTRAINT `issue_comments_linked_fk2` FOREIGN KEY (user_id) REFERENCES GH_users(id);
+DROP TABLE issue_comments;
+
+
+
+-- filter issue_events table --> keep only issue_id that are in the issues_linked table
+CREATE TABLE issue_events_linked AS
+SELECT * FROM issue_events
+WHERE issue_id IN (SELECT id FROM issues_linked);
+
+
+ALTER TABLE issue_events_linked ADD CONSTRAINT `issue_events_linked_fk1` FOREIGN KEY (issue_id) REFERENCES issues_linked(id);
+ALTER TABLE issue_events_linked ADD CONSTRAINT `issue_events_linked_fk2` FOREIGN KEY (actor_id) REFERENCES GH_users(id);
+DROP TABLE issue_events;
+
+
+
+-- filter issue_labels table --> keep only issue_id and label_id that are in the issues_linked and repo_labels_linked tables
+CREATE TABLE issue_labels_linked AS
+SELECT * FROM issue_labels
+WHERE issue_id IN (SELECT id FROM issues_linked);
+
+ALTER TABLE issue_labels_linked ADD CONSTRAINT `issue_labels_linked_pk` PRIMARY KEY (issue_id, label_id);
+ALTER TABLE issue_labels_linked ADD CONSTRAINT `issue_labels_linked_fk1` FOREIGN KEY (label_id) REFERENCES repo_labels_linked(id);
+ALTER TABLE issue_labels_linked ADD CONSTRAINT `issue_labels_linked_fk2` FOREIGN KEY (issue_id) REFERENCES issues_linked(id);
+DROP TABLE issue_labels;
